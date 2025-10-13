f6b7d1a196d36a82d4043008279a52ef73ecd961
zaddmc <davidbk927@gmail.com>
2025-10-13 15:26:33 +0200

    week is correct now, some server status
    serverstatus pinging the wron thing

--- scripts/serverstatus.sh
+++ scripts/serverstatus.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-if ping -c 1 homeserver > /dev/null 2>&1; then
-    echo $(jq -n --arg ip "Home Server IP: $(getent hosts homeserver | awk '{ print $1}')" '{"text": "󰗠", "class": "online", "tooltip": $ip}')
+target="clankersrefuge.dk"
+
+if ping -c 1 $target > /dev/null 2>&1; then
+    echo $(jq -n --arg ip "Home Server IP: $(getent hosts $target | awk '{ print $1}')" '{"text": "󰗠", "class": "online", "tooltip": $ip}')
 else
     echo '{"text": "", "class":"offline"}' 
 fi
