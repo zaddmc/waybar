3efdd4b8a363c67230ff5a9c96903a7edc61c618
zaddmc <davidbk927@gmail.com>
2025-11-03 09:19:09 +0100

    idk

--- scripts/playerctl-wrapper.sh
+++ scripts/playerctl-wrapper.sh
@@ -3,7 +3,7 @@
 # Show default state first
 echo '{"text": "Nothing playing", "tooltip": "No media players running", "alt": "none", "class": "stopped"}'
 
-playerctl -a metadata --format '{"text": "{{title}} - {{artist}}", "tooltip": "{{playerName}} : {{artist}} - {{title}}", "alt": "{{status}}", "class": "{{status}}"}' -F 2>/dev/null | \
+playerctl -a metadata --format '{"text": "{{title}} — {{artist}}", "tooltip": "{{playerName}} : {{artist}} - {{title}}", "alt": "{{status}}", "class": "{{status}}"}' -F 2>/dev/null | \
 while read -r line; do
     if [[ -z "$line" ]]; then
         echo '{"text": "Nothing playing", "tooltip": "No media players running", "alt": "none", "class": "stopped"}'
--- scripts/serverstatus.sh
+++ scripts/serverstatus.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 target="clankersrefuge.dk"
 
-if ping -c 1 $target > /dev/null 2>&1; then
+if ping -c 2 $target > /dev/null 2>&1; then
     echo $(jq -n --arg ip "Home Server IP: $(getent hosts $target | awk '{ print $1}')" '{"text": "󰗠", "class": "online", "tooltip": $ip}')
 else
     echo '{"text": "", "class":"offline"}' 
